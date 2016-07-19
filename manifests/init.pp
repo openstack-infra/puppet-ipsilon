ab29f0c72220a4eeb3e7a4dd0e3ef5060a5a6e54
James E. Blair <jeblair@redhat.com>
2016-07-19 09:51:38 -0700

    Add module boilerplate
    Change-Id: Icdab79594f9ded5026900a15862e6b057d9d7b1f

---
+++ manifests/init.pp
@@ -0,0 +1,10 @@
+# == Class: ipsilon
+#
+# Full description of class ipsilon here.
+#
+# === Parameters
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#
+class ipsilon {}
