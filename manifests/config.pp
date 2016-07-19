0d4a0be8a9739761853fcd472eaee3cbbce1f98b
James E. Blair <jeblair@redhat.com>
2016-07-19 09:51:38 -0700

    Add module boilerplate
    Change-Id: Icdab79594f9ded5026900a15862e6b057d9d7b1f

---
+++ manifests/config.pp
@@ -0,0 +1,31 @@
+# == Class: ipsilon::config
+#
+# This class is used to manage arbitrary ipsilon configurations.
+#
+# === Parameters
+#
+# [*ipsilon_config*]
+#   (optional) Allow configuration of arbitrary ipsilon configurations.
+#   The value is an hash of ipsilon_config resources. Example:
+#   { 'DEFAULT/foo' => { value => 'fooValue'},
+#     'DEFAULT/bar' => { value => 'barValue'}
+#   }
+#   In yaml format, Example:
+#   ipsilon_config:
+#     DEFAULT/foo:
+#       value: fooValue
+#     DEFAULT/bar:
+#       value: barValue
+#
+#   NOTE: The configuration MUST NOT be already handled by this module
+#   or Puppet catalog compilation will fail with duplicate resources.
+#
+class ipsilon::config (
+  $ipsilon_config = {},
+) {
+
+  validate_hash($ipsilon_config)
+
+  create_resources('ipsilon_config', $ipsilon_config)
+}
+
